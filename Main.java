9bc80a15ee7ebca424a3d6bb748163025e08c400
priyanshu <priyanshu@e6x.io>
2024-06-29 13:46:09 +0530

    second commit on feature branch

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("main branch");
         System.out.println("1 commit feature");
+        System.out.println("2 commit feature");
     }
 }
